485fdda2809660b44426812cf3581fefad3deda2
Rakesh <krakesh7788@gmail.com>
2024-03-09 19:31:31 +0530

    linkedin private error removed

--- app/linkedin/page.jsx
+++ app/linkedin/page.jsx
@@ -1,33 +1,33 @@
-"use client"
-import JobFeed from "@/components/JobFeed"
-import { useEffect, useState } from "react"
-import scheduleCronJob, {
-  fetchLinkedinJobs,
-} from "../scrappers/linkedinScrapper/fetchData"
+// "use client"
+// import JobFeed from "@/components/JobFeed"
+// import { useEffect, useState } from "react"
+// import scheduleCronJob, {
+//   fetchLinkedinJobs,
+// } from "../scrappers/linkedinScrapper/fetchData"
 
-const page = () => {
-  const [data, setData] = useState([])
+// const page = () => {
+//   const [data, setData] = useState([])
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        // scheduleCronJob()
-        const res = await fetch("/api/linkedin")
-        const data = await res.json()
-        console.log(data)
-        setData(data)
-      } catch (error) {
-        console.log(error)
-      }
-    }
-    fetchData()
-  }, [])
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         // scheduleCronJob()
+//         const res = await fetch("/api/linkedin")
+//         const data = await res.json()
+//         console.log(data)
+//         setData(data)
+//       } catch (error) {
+//         console.log(error)
+//       }
+//     }
+//     fetchData()
+//   }, [])
 
-  return (
-    <div>
-      <JobFeed data={data} />
-    </div>
-  )
-}
+//   return (
+//     <div>
+//       <JobFeed data={data} />
+//     </div>
+//   )
+// }
 
-export default page
+// export default page
