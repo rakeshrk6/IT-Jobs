4c00cf70353233f6091818db0943d0c07759af24
Rakesh <krakesh7788@gmail.com>
2024-03-11 23:54:54 +0530

    cron route error solved

--- app/api/cron/route.js
+++ app/api/cron/route.js
@@ -1,18 +1,6 @@
 import { fetchGoogleJobs } from "@/app/scrappers/googleScrapper/fetchData"
 import { fetchInternshalaData } from "@/app/scrappers/internshalaScrapper/fetchData"
 
-import { NextResponse } from "next/server"
-
-export async function GET() {
-  try {
-    await fetchInternshalaData()
-    await fetchGoogleJobs()
-    return NextResponse.json({ ok: true })
-  } catch (error) {
-    return new Response("Failed to fetch all prompts", { status: 500 })
-  }
-}
-
 export const GET = async (req, res) => {
   try {
     await fetchInternshalaData()
