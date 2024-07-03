6ddba05713ae903cf62d16bcf170018f7994bb49
Rakesh <krakesh7788@gmail.com>
2024-07-03 23:01:05 +0530

    api to hit cron-jobs

--- netlify/functions/fetchData.js
+++ netlify/functions/fetchData.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // import { fetchGoogleJobs } from "../../app/scrappers/googleScrapper/fetchData"
 // import { fetchInternshalaData } from "../../app/scrappers/internshalaScrapper/fetchData"
 
@@ -17,23 +16,3 @@
 //     }
 //   }
 // }
-=======
-import { fetchGoogleJobs } from "../../app/scrappers/googleScrapper/fetchData"
-import { fetchInternshalaData } from "../../app/scrappers/internshalaScrapper/fetchData"
-
-export const handler = async (event, context) => {
-  try {
-    await fetchInternshalaData()
-    await fetchGoogleJobs()
-    return {
-      statusCode: 200,
-      body: "Data fetched",
-    }
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: "Failed to fetch all prompts",
-    }
-  }
-}
->>>>>>> d8bb546f2dfa72ec8f6a3eb30222fb20e8f2d816
