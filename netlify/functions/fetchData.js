30fe4aba12ee6f2f8cd3b0673a183dff4de8d4bb
Rakesh <krakesh7788@gmail.com>
2024-07-03 23:05:37 +0530

    netlify schedule func

--- netlify/functions/fetchData.js
+++ netlify/functions/fetchData.js
@@ -1,18 +1,13 @@
-// import { fetchGoogleJobs } from "../../app/scrappers/googleScrapper/fetchData"
-// import { fetchInternshalaData } from "../../app/scrappers/internshalaScrapper/fetchData"
+import { fetchGoogleJobs } from "../../app/scrappers/googleScrapper/fetchData"
+import { fetchInternshalaData } from "../../app/scrappers/internshalaScrapper/fetchData"
 
-// export const handler = async (event, context) => {
-//   try {
-//     await fetchInternshalaData()
-//     await fetchGoogleJobs()
-//     return {
-//       statusCode: 200,
-//       body: "Data fetched",
-//     }
-//   } catch (error) {
-//     return {
-//       statusCode: 500,
-//       body: "Failed to fetch all prompts",
-//     }
-//   }
-// }
+export const handler = async (event, context) => {
+  try {
+    // await fetchInternshalaData()
+    // await fetchGoogleJobs()
+    console.log("cron jobs called")
+    return new Response("Data fetched", { status: 200 })
+  } catch (error) {
+    return new Response("Failed to fetch all prompts", { status: 500 })
+  }
+}
