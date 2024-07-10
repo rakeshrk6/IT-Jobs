7c7d769861c5d888cdc2290cff7d2bccfc5895cc
Rakesh <krakesh7788@gmail.com>
2024-07-11 01:34:09 +0530

    remove node cron

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,7 @@
 import { companyData } from "../assets/companyCardData"
 import CompanyCard from "../components/CompanyCard"
-import cron from "node-cron"
-import { fetchInternshalaData } from "../scrappers/internshalaScrapper/fetchData"
 
 const page = () => {
-  cron.schedule("* * * * *", async () => {
-    console.log("started")
-    const internshalaJobs = await fetchInternshalaData()
-    console.log("job fetched", internshalaJobs)
-  })
-
   return (
     <div className="flex flex-wrap gap-6">
       {companyData.map((item) => (
