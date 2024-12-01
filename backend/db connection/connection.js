83790847a3a2eff78c38f17443657dabbaab897c
Rakesh <krakesh7788@gmail.com>
2024-12-01 14:47:13 +0530

    corrected

--- backend/db connection/connection.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose"
-
-export async function connect() {
-  await mongoose.connect(process.env.ATLAS_URL, {
-    dbName: "Job-app",
-  })
-  console.log("database connected")
-}
